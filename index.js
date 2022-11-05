122ca38c390fec4d59afb3feb803e1c6cf5ace64
Iury <iurysena74@gmail.com>
2022-11-05 13:42:56 -0300

    upando projeto para o github

---
+++ index.js
@@ -0,0 +1,49 @@
+const inputDigit = document.querySelector("form input"),
+convertButton = document.querySelector("form button"),
+inputResult = document.querySelector(".result input");
+
+inputDigit.addEventListener("keydown", function(e){
+    if(e.key > 1 || e.key < 0) {
+        alert("Não é permitido outros números além de 0 e 1!")
+    }
+})
+
+inputDigit.addEventListener("input", function(){
+    if(inputDigit.value == "") {
+        inputResult.value = ""
+    }
+})
+
+let arrayResult = []
+var soma = 0;
+
+function convertUnits(e) {
+    e.preventDefault();
+
+    if(inputDigit.value == "") return alert("Digite algum número!")
+
+    var newString = ""
+
+    for(let i = inputDigit.value.length - 1; i >= 0; i--) {
+        newString += inputDigit.value[i]
+    }
+
+    for(let i = 0; i < inputDigit.value.length; i++) {
+        let res = newString[i] * Math.pow(2, i)
+        arrayResult.push(res)
+    }
+
+    for(var i = 0; i < arrayResult.length; i++) {
+         soma += arrayResult[i];
+     }
+
+    inputResult.value = soma.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    arrayResult = []
+    soma = 0
+    console.log(soma)
+    console.log(arrayResult)
+
+    
+}
+
+convertButton.addEventListener("click", convertUnits)
